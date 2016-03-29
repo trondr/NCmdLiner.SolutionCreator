e3f73d58da6e162e7a974b5e91a42832ba4c93d4
trondr <trondr@outlook.com>
2016-03-30 00:04:47 +0200

    NCmdLiner.SolutionCreator.1.0.16089.45: Updated tools.

--- src/NCmdLiner.SolutionCreator/Properties/AssemblyInfo.cs
+++ src/NCmdLiner.SolutionCreator/Properties/AssemblyInfo.cs
@@ -20,8 +20,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("NCmdLiner.SolutionCreator")]
 [assembly: AssemblyCopyright("Copyright © <github.com.trondr> 2016")]
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyInformationalVersion("1.0.16055.")]
-[assembly: AssemblyVersion("1.0.16055.43")]
-[assembly: AssemblyFileVersion("1.0.16055.43")]
+[assembly: AssemblyInformationalVersion("1.0.16089.")]
+[assembly: AssemblyVersion("1.0.16089.45")]
+[assembly: AssemblyFileVersion("1.0.16089.45")]
 
 
